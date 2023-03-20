d4dac02324a87eded771af9e137f9ea29cda46b4
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-21 00:55:12 +0900

    initial commit

---
+++ src/main/java/com/example/handoverbackend/HandOverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.handoverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandOverBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandOverBackendApplication.class, args);
+    }
+
+}
