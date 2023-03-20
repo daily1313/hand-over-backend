91dab698fe7636de1102b98ed4e118435ba94b92
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-21 02:10:13 +0900

    feat : 스프링 시큐리티 설정파일 추가

---
+++ src/main/java/com/example/handoverbackend/config/security/WebConfig.java
@@ -0,0 +1,36 @@
+package com.example.handoverbackend.config.security;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final MessageSource messageSource;
+
+    private String location = "/Users/kimseungbeom/Desktop/image/";
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + location)
+                .setCacheControl(CacheControl.maxAge(Duration.ofHours(1L)).cachePublic());
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+}
