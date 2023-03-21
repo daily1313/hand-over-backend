d1b47ae1ee45921e3c2bef7afbf1b359479aa6ba
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-21 15:28:16 +0900

    feat : HandOverBackendApplication.java

--- src/main/java/com/example/handoverbackend/HandOverBackendApplication.java
+++ src/main/java/com/example/handoverbackend/HandOverBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.handoverbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HandOverBackendApplication {
 
     public static void main(String[] args) {
