39e366325857a73db26475d00723ece0efc44e48
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-25 17:14:36 +0900

    feat: 즐겨찾기 entity추가

---
+++ src/main/java/com/example/handoverbackend/domain/board/Favorite.java
@@ -0,0 +1,42 @@
+package com.example.handoverbackend.domain.board;
+
+import com.example.handoverbackend.domain.common.BaseEntity;
+import com.example.handoverbackend.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "favorite_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    public static Favorite createFavorite(Board board, Member member){
+        return new Favorite(board, member);
+    }
+
+    private Favorite(Board board, Member member) {
+        this.board = board;
+        this.member = member;
+    }
+}
