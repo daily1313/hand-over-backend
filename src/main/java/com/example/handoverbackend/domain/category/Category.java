83f839f822a1f6b43a66f2ef3fb529a1eeab54ab
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-25 17:12:03 +0900

    feat: 카테고리 추가

---
+++ src/main/java/com/example/handoverbackend/domain/category/Category.java
@@ -0,0 +1,26 @@
+package com.example.handoverbackend.domain.category;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public static Category createCategory(String name){
+        return new Category(name);
+    }
+
+    private Category(String name) {
+        this.name = name;
+    }
+}
