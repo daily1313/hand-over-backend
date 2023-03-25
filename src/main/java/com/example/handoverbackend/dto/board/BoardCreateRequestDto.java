e2e3de3566e2428cd775c2908432201e8d0a26af
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-25 17:10:01 +0900

    feat: 게시판 requestDto 추가

---
+++ src/main/java/com/example/handoverbackend/dto/board/BoardCreateRequestDto.java
@@ -0,0 +1,31 @@
+package com.example.handoverbackend.dto.board;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApiOperation(value = "게시글 생성 요청")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardCreateRequestDto {
+
+    @ApiModelProperty(value = "게시글 제목", notes = "게시글 제목을 입력해주세요", required = true, example = "게시글 제목")
+    @NotBlank(message = "게시글 제목을 입력해주세요.")
+    private String title;
+
+    @ApiModelProperty(value = "게시글 본문", notes = "게시글 본문을 입력해주세여", required = true, example = "게시글 본문")
+    @NotBlank(message = "게시글 본문을 입력해주세요.")
+    private String content;
+
+    @ApiModelProperty(value = "이미지", notes = "이미지를 첨부해주세요.")
+    private List<MultipartFile> images = new ArrayList<>();
+}
