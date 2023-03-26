97eeb56f1bd19a2433b20941bfca2d556483f899
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-26 15:50:06 +0900

    feat: 카테고리를 이름으로 찾는 쿼리 추가

--- src/main/java/com/example/handoverbackend/repository/category/CategoryRepository.java
+++ src/main/java/com/example/handoverbackend/repository/category/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.example.handoverbackend.repository.category;
 import com.example.handoverbackend.domain.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Optional<Category> findByName(String name);
 }
