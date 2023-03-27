aa607a71ae28febe3c37b4f1eaf94d8bcaeb2092
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-27 14:07:11 +0900

    feat: 게시판 신고 리포지토리 추가

---
+++ src/main/java/com/example/handoverbackend/repository/report/BoardReportRepository.java
@@ -0,0 +1,15 @@
+package com.example.handoverbackend.repository.report;
+
+import com.example.handoverbackend.domain.board.Board;
+import com.example.handoverbackend.domain.member.Member;
+import com.example.handoverbackend.domain.report.BoardReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardReportRepository extends JpaRepository<BoardReport, Long> {
+
+    boolean existsByReporterAndReported(Member reporter, Board reported);
+
+    List<BoardReport> findAllByReported(Board reported);
+}
