a60ce2dae502f8f4f6c6e2148597b25383eefaef
kimtaesoo <kimtaesoo7@naver.com>
2023-04-09 16:30:22 +0900

    feat: 댓글 Dto 추가

---
+++ src/main/java/com/example/handoverbackend/dto/comment/CommentCreateRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.handoverbackend.dto.comment;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@ApiOperation(value = "댓글 생성 요청")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentCreateRequestDto {
+
+    @ApiModelProperty(value = "게시글 번호", notes = "게시글 번호를 입력해주세요.", required = true, example = "1")
+    @NotNull(message = "게시글 번호를 입력해주세요.")
+    @Positive(message = "게시글 번호를 입력해주세요.")
+    private Long boardId;
+
+    @ApiModelProperty(value = "댓글", notes = "댓글을 입력해주세요.", required = true, example = "example comment")
+    @NotBlank(message = "댓글을 입력해주세요.")
+    private String content;
+}
---
+++ src/main/java/com/example/handoverbackend/dto/comment/CommentResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.handoverbackend.dto.comment;
+
+import com.example.handoverbackend.domain.comment.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponseDto {
+
+    private String writer;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public static CommentResponseDto toDto(Comment comment) {
+        return new CommentResponseDto(comment.getMember().getNickname(),
+            comment.getContent(), comment.getCreatedAt(), comment.getModifiedAt());
+    }
+}
