1a74c90a9c57203dcd4d582d45741df7bd958dec
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-05-05 04:50:32 +0900

    feat : MatchService, MatchController Test 추가

--- src/test/java/com/example/handoverbackend/controller/match/MatchControllerTest.java
+++ src/test/java/com/example/handoverbackend/controller/match/MatchControllerTest.java
@@ -1,8 +1,8 @@
-package com.example.handoverbackend.controller.ticket;
+package com.example.handoverbackend.controller.match;
 
 import static com.example.handoverbackend.factory.AuthenticationMaker.createAuthentication;
+import static com.example.handoverbackend.factory.MatchMaker.createMatch;
 import static com.example.handoverbackend.factory.MemberMaker.createMember;
-import static com.example.handoverbackend.factory.TicketMaker.createTicket;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.refEq;
@@ -13,18 +13,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.example.handoverbackend.domain.ticket.Ticket;
-import com.example.handoverbackend.dto.ticket.TicketEditRequestDto;
+import com.example.handoverbackend.domain.match.Match;
+import com.example.handoverbackend.dto.match.MatchEditRequestDto;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 
 import com.example.handoverbackend.domain.member.Member;
-import com.example.handoverbackend.domain.ticket.Category;
-import com.example.handoverbackend.dto.ticket.TicketCreateRequestDto;
+import com.example.handoverbackend.domain.match.Category;
+import com.example.handoverbackend.dto.match.MatchCreateRequestDto;
 import com.example.handoverbackend.repository.MemberRepository;
-import com.example.handoverbackend.service.ticket.TicketService;
+import com.example.handoverbackend.service.match.MatchService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,17 +40,16 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(MockitoExtension.class)
-public class TicketControllerTest {
+public class MatchControllerTest {
 
     @InjectMocks
-    TicketController ticketController;
+    MatchController matchController;
 
     @Mock
-    TicketService ticketService;
+    MatchService matchService;
 
     @Mock
     MemberRepository memberRepository;
@@ -59,14 +59,16 @@ public class TicketControllerTest {
 
     @BeforeEach
     void beforeEach() {
-        mockMvc = MockMvcBuilders.standaloneSetup(ticketController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(matchController).build();
     }
 
-    @DisplayName("티켓 판매글 작성 테스트")
+    @DisplayName("매칭글 작성 테스트")
     @Test
     void writeTicketPostTest() throws Exception {
         //given
-        TicketCreateRequestDto req = new TicketCreateRequestDto(Category.공연, "아이유 콘서트", "경기 용인시", LocalDate.parse("2023-04-12"), LocalDate.parse("2023-04-12"), "공연", 150000);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        MatchCreateRequestDto req = new MatchCreateRequestDto(Category.노인돌봄 , "노인 돌보기", "경기 용인시 처인구",
+                LocalDateTime.parse("2023-04-12 12:00", formatter), LocalDateTime.parse("2023-04-12 12:00", formatter), "돌보기", 150000, "거동이 불편하니 조심하세요");
         Member member = createMember();
         Authentication authentication = createAuthentication(member);
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -74,46 +76,46 @@ public class TicketControllerTest {
 
         //when
         mockMvc.perform(
-                post("/api/tickets")
+                post("/api/matches")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(req))
         ).andExpect(status().isCreated());
 
         //then
-        verify(ticketService).writeTicketPost(refEq(member), refEq(req));
+        verify(matchService).writeMatchPost(refEq(member), refEq(req));
     }
 
-    @DisplayName("티켓 판매글 전체 조회 테스트")
+    @DisplayName("매칭글 전체 조회 테스트")
     @Test
-    void getAllTicketPostsWithPagingTest() throws Exception {
+    void getAllMatchesPostsWithPagingTest() throws Exception {
         //given
         Integer page = 0;
 
         //when
         mockMvc.perform(
-                get("/api/tickets")
+                get("/api/matches")
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).getAllTicketPostsWithPaging(0);
+        verify(matchService).getAllMatchesPostsWithPaging(page);
     }
 
-    @DisplayName("티켓 판매글 단건 조회 테스트")
+    @DisplayName("매칭글 단건 조회 테스트")
     @Test
-    void getTicketPostTest() throws Exception {
+    void getMatchPostTest() throws Exception {
         //given
         Long id = 1L;
 
         //when
         mockMvc.perform(
-                get("/api/tickets/{id}", id)
+                get("/api/matches/{id}", id)
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).getTicketPost(1L);
+        verify(matchService).getMatchPost(1L);
     }
 
-    @DisplayName("티켓 판매 상태 판매완료로 변경 테스트")
+    @DisplayName("매칭글 상태 변경 테스트")
     @Test
     void changeTicketSoldOutStatusTest() throws Exception {
         //given
@@ -125,59 +127,41 @@ public class TicketControllerTest {
 
         //when
         mockMvc.perform(
-                patch("/api/tickets/{id}/edit/soldOut", id)
+                patch("/api/matches/{id}/edit/matchStatus", id)
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).changeTicketSoldOutStatus(member, id);
+        verify(matchService).changeMatchStatus(member, id);
     }
 
-    @DisplayName("티켓 판매 상태 판매완료로 변경 테스트")
+    @DisplayName("매칭글 수정 테스트")
     @Test
-    void changeTicketOnSaleStatusTest() throws Exception {
+    void editMatchPostTest() throws Exception {
         //given
         Long id = 1L;
         Member member = createMember();
+        Match match = createMatch(member);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        MatchEditRequestDto req = new MatchEditRequestDto(Category.노인돌봄 , "노인 배웅", "경기 용인시 처인구",
+                LocalDateTime.parse("2023-04-12 12:00", formatter), LocalDateTime.parse("2023-04-12 12:00", formatter), "돌보기", 150000, "거동이 불편하니 조심하세요");
+        match.editMatchInfo(req);
         Authentication authentication = createAuthentication(member);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         given(memberRepository.findByUsername(authentication.getName())).willReturn(Optional.of(member));
 
         //when
         mockMvc.perform(
-                patch("/api/tickets/{id}/edit/onSale", id)
-        ).andExpect(status().isOk());
-
-        //then
-        verify(ticketService).changeTicketOnSaleStatus(member, id);
-    }
-
-    @DisplayName("티켓 판매글 수정 테스트")
-    @Test
-    void editTicketPostTest() throws Exception {
-        //given
-        Long id = 1L;
-        Member member = createMember();
-        Ticket ticket = createTicket(member);
-        TicketEditRequestDto req = new TicketEditRequestDto(Category.공연, "엑소 콘서트", "경기 용인시", LocalDate.parse("2023-04-12"), LocalDate.parse("2023-04-12"), "공연", 150000);
-        ticket.editTicketInfo(req);
-        Authentication authentication = createAuthentication(member);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        given(memberRepository.findByUsername(authentication.getName())).willReturn(Optional.of(member));
-
-
-        //when
-        mockMvc.perform(
-                patch("/api/tickets/{id}", 1L)
+                patch("/api/matches/{id}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(req)))
                 .andExpect(status().isOk());
 
         //then
-        verify(ticketService).editTicketPost(refEq(member), refEq(req), any());
-        assertThat(ticket.getTicketName()).isEqualTo("엑소 콘서트");
+        verify(matchService).editMatchPost(refEq(member), refEq(req), any());
+        assertThat(match.getMatchName()).isEqualTo("노인 배웅");
     }
 
-    @DisplayName("티켓 판매글 즐겨찾기 테스트")
+    @DisplayName("매칭글 즐겨찾기 테스트")
     @Test
     void updateFavoriteTicketPost() throws Exception {
         //given
@@ -189,14 +173,14 @@ public class TicketControllerTest {
 
         //when
         mockMvc.perform(
-                post("/api/tickets/{id}/favorites", id)
+                post("/api/matches/{id}/favorites", id)
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).updateFavoriteTicketPost(id, member);
+        verify(matchService).updateMatchFavoritePost(id, member);
     }
 
-    @DisplayName("티켓 판매글 즐겨찾기 목록 테스트")
+    @DisplayName("매칭글 즐겨찾기 목록 테스트")
     @Test
     void findFavoriteTicketPosts() throws Exception {
         //given
@@ -208,42 +192,44 @@ public class TicketControllerTest {
 
         //when
         mockMvc.perform(
-                get("/api/tickets/favorites")
+                get("/api/matches/favorites")
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).findFavoriteTicketPosts(page, member);
+        verify(matchService).findFavoriteMatchesPosts(page, member);
     }
 
-    @DisplayName("티켓 판매글 검색 테스트(주소)")
+    @DisplayName("매칭글 검색 테스트(주소)")
     @Test
-    void getAllTicketPostWithPagingBySearchingAddressTest() throws Exception {
+    void getAllMatchesPostWithPagingBySearchingAddressTest() throws Exception {
         //given
         String address = "용인";
         Integer page = 0;
 
         //when
         mockMvc.perform(
-                get("/api/tickets/search/address/{address}", address)
+                get("/api/matches/search/address")
+                        .param("address", address)
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).getAllTicketPostWithPagingBySearchingAddress(address, page);
+        verify(matchService).getAllMatchesPostWithPagingBySearchingAddress(address, page);
     }
 
-    @DisplayName("티켓 판매글 검색 테스트(이름)")
+    @DisplayName("매칭글 검색 테스트(이름)")
     @Test
-    void getAllTicketPostWithPagingBySearchingTicketNameTest() throws Exception {
+    void getAllMatchesPostWithPagingBySearchingMatchNameTest() throws Exception {
         //given
-        String ticketName = "아이유";
+        String matchName = "노인 배웅";
         Integer page = 0;
 
         //when
         mockMvc.perform(
-                get("/api/tickets/search/ticketName/{ticketName}", ticketName)
+                get("/api/matches/search/matchName")
+                        .param("matchName", matchName)
         ).andExpect(status().isOk());
 
         //then
-        verify(ticketService).getAllTicketPostWithPagingBySearchingTicketName(ticketName, page);
+        verify(matchService).getAllMatchesPostWithPagingBySearchingMatchName(matchName, page);
     }
 }
---
+++ src/test/java/com/example/handoverbackend/service/match/MatchServiceTest.java
@@ -0,0 +1,226 @@
+package com.example.handoverbackend.service.match;
+
+import static com.example.handoverbackend.factory.MatchMaker.createMatch;
+import static com.example.handoverbackend.factory.MemberMaker.createMember;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.example.handoverbackend.domain.favorite.MatchFavorite;
+import com.example.handoverbackend.domain.match.Match;
+import com.example.handoverbackend.domain.member.Member;
+import com.example.handoverbackend.domain.match.Category;
+import com.example.handoverbackend.dto.match.MatchCreateRequestDto;
+import com.example.handoverbackend.dto.match.MatchFindAllWithPagingResponseDto;
+import com.example.handoverbackend.dto.match.MatchResponseDto;
+import com.example.handoverbackend.repository.match.MatchFavoriteRepository;
+import com.example.handoverbackend.repository.match.MatchRepository;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+@ExtendWith(MockitoExtension.class)
+class MatchServiceTest {
+
+    @InjectMocks
+    MatchService matchService;
+
+    @Mock
+    MatchRepository matchRepository;
+
+    @Mock
+    MatchFavoriteRepository matchFavoriteRepository;
+
+    @DisplayName("매칭글 작성 테스트")
+    @Test
+    void writeMatchPostTest() {
+        //given
+        Match match = createMatch();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        MatchCreateRequestDto req = new MatchCreateRequestDto(Category.노인돌봄 , "노인 돌보기", "경기 용인시 처인구",
+                LocalDateTime.parse("2023-04-12 12:00", formatter), LocalDateTime.parse("2023-04-12 12:00", formatter), "돌보기", 150000, "거동이 불편하니 조심하세요");
+        given(matchRepository.save(any())).willReturn(match);
+
+        //when
+        MatchResponseDto matchResponseDto = matchService.writeMatchPost(match.getSeller(), req);
+
+        //then
+        assertThat(matchResponseDto.getAddress()).isEqualTo("경기 용인시 처인구");
+    }
+
+    @DisplayName("매칭글 전체 조회 테스트(최신순)")
+    @Test
+    void getAllMatchesPostsWithPagingTest() {
+        //given
+        PageRequest pageRequest = PageRequest.of(1, 10, Sort.by("id").descending());
+        List<Match> matches = List.of(createMatch(), createMatch());
+        Page<Match> allMatches = new PageImpl<>(matches);
+        given(matchRepository.findAll(pageRequest)).willReturn(allMatches);
+
+        //when
+        MatchFindAllWithPagingResponseDto result = matchService.getAllMatchesPostsWithPaging(1);
+
+        //then
+        assertThat(result.getMatches().size()).isEqualTo(2);
+    }
+
+    @DisplayName("매칭글 조회 테스트(가격 오름차순)")
+    @Test
+    void getAllMatchesPostWithPagingOrderByPriceAscTest() {
+        //given
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        Match match = new Match(createMember(), Category.반려동물, "러시안 블루 밥주기", "경기 시흥시", LocalDateTime.parse("2023-04-15 10:00", formatter),LocalDateTime.parse("2023-04-15 11:00", formatter), "동물 돌보미", 50000, "사나우니까 조심좀해주세요", false);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("price").ascending());
+        List<Match> matches = List.of(match, createMatch());
+        given(matchRepository.findAllByOrderByPrice(pageRequest)).willReturn(new PageImpl<>(matches, pageRequest, matches.size()));
+
+        //when
+        MatchFindAllWithPagingResponseDto result = matchService.getAllMatchesPostWithPagingOrderByPriceAsc(0);
+
+        //then
+        assertThat(result.getMatches().get(0).getPrice()).isEqualTo(50000);
+    }
+
+    @DisplayName("매칭글 조회 테스트(가격 내림차순)")
+    @Test
+    void getAllMatchesPostWithPagingOrderByPriceDescTest() {
+        //given
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        Match match1 = new Match(createMember(), Category.반려동물, "러시안 블루 밥주기", "경기 시흥시", LocalDateTime.parse("2023-04-15 10:00", formatter),LocalDateTime.parse("2023-04-15 10:00", formatter), "동물 돌보미", 50000, "사나우니까 조심좀해주세요", false);
+        Match match2 = new Match(createMember(), Category.노인돌봄, "노인 책 읽어주기", "제주도", LocalDateTime.parse("2023-05-04 10:00", formatter), LocalDateTime.parse("2023-05-04 11:00", formatter), "노인 돌보미", 150000, "거동이 불편합니다",false);
+        List<Match> matches = List.of(match1, match2);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("price").descending());
+        given(matchRepository.findAllByOrderByPriceDesc(pageRequest)).willReturn(new PageImpl<>(matches, pageRequest, matches.size()));
+
+        //when
+        MatchFindAllWithPagingResponseDto result = matchService.getAllMatchesPostWithPagingOrderByPriceDesc(0);
+
+        //then
+        assertThat(result.getMatches().get(0).getPrice()).isEqualTo(50000);
+    }
+
+    @DisplayName("매칭글 검색 조회 테스트(티켓이름으로 검색)")
+    @Test
+    void getAllMatchesPostWithPagingBySearchingMatchNameTest() {
+        String matchName = "러시안";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        Match match1 = new Match(createMember(), Category.반려동물, "러시안 블루 밥주기", "경기 시흥시", LocalDateTime.parse("2023-04-15 10:00", formatter),LocalDateTime.parse("2023-04-15 10:00", formatter), "동물 돌보미", 50000, "사나우니까 조심좀해주세요", false);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("id").descending());
+        List<Match> matches = List.of(match1);
+        given(matchRepository.findAllByMatchNameContaining(matchName, pageRequest)).willReturn(new PageImpl<>(
+                matches));
+
+        //when
+        MatchFindAllWithPagingResponseDto result = matchService.getAllMatchesPostWithPagingBySearchingMatchName(matchName, 0);
+
+        //then
+        assertThat(result.getMatches().size()).isEqualTo(1);
+        assertThat(result.getMatches().get(0).getTicketName()).isEqualTo(match1.getMatchName());
+    }
+
+    @DisplayName("매칭글 검색 조회 테스트(주소로 검색)")
+    @Test
+    void getAllMatchesPostWithPagingBySearchingAddressTest() {
+        String address = "시흥";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        Match match1 = new Match(createMember(), Category.반려동물, "러시안 블루 밥주기", "경기 시흥시", LocalDateTime.parse("2023-04-15 10:00", formatter),LocalDateTime.parse("2023-04-15 10:00", formatter), "동물 돌보미", 50000, "사나우니까 조심좀해주세요", false);
+        Match match2 = new Match(createMember(), Category.노인돌봄, "노인 책 읽어주기", "제주도", LocalDateTime.parse("2023-05-04 10:00", formatter), LocalDateTime.parse("2023-05-04 11:00", formatter), "노인 돌보미", 150000, "거동이 불편합니다",false);
+        List<Match> matches = List.of(match1);
+
+        given(matchRepository.findAllByAddressContaining(address, PageRequest.of(0, 10, Sort.by("id").descending()))).willReturn(new PageImpl<>(
+                matches));
+
+        //when
+        MatchFindAllWithPagingResponseDto result = matchService.getAllMatchesPostWithPagingBySearchingAddress(address, 0);
+
+        //then
+        assertThat(result.getMatches().size()).isEqualTo(1);
+        assertThat(result.getMatches().get(0).getAddress()).isEqualTo(match1.getAddress());
+    }
+
+    @DisplayName("매칭글 즐겨찾기 테스트")
+    @Test
+    void createFavoriteMatchPostTest() {
+        //given
+        Member member = createMember();
+        Match match = createMatch();
+
+        given(matchRepository.findById(1L)).willReturn(Optional.of(match));
+        given(matchFavoriteRepository.existsByMatchAndMember(match, member)).willReturn(false);
+
+        //when
+        String result = matchService.updateMatchFavoritePost(1L, member);
+
+        //then
+        assertThat(result).isEqualTo("즐겨찾기를 하였습니다.");
+        verify(matchFavoriteRepository).save(any());
+    }
+
+    @DisplayName("매칭글 즐겨찾기 취소 테스트")
+    @Test
+    void deleteMatchFavoritePostTest() {
+        //given
+        Member member = createMember();
+        Match match = createMatch();
+        MatchFavorite matchFavorite = MatchFavorite.createFavorite(match, member);
+
+        given(matchRepository.findById(1L)).willReturn(Optional.of(match));
+        given(matchFavoriteRepository.existsByMatchAndMember(match, member)).willReturn(true);
+        given(matchFavoriteRepository.findByMatchAndMember(match, member)).willReturn(Optional.of(matchFavorite));
+
+        //when
+        String result = matchService.updateMatchFavoritePost(1L, member);
+
+        //then
+        assertThat(result).isEqualTo("즐겨찾기가 해제되었습니다.");
+        verify(matchFavoriteRepository).delete(any());
+    }
+
+    @DisplayName("매칭글 전체목록 조회 테스트")
+    @Test
+    void findFavoriteMatchesPostsTest() {
+        //given
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("id").descending());
+        Member member = createMember();
+        List<MatchFavorite> favorites = new ArrayList<>();
+        favorites.add(MatchFavorite.createFavorite(createMatch(member), member));
+        Page<MatchFavorite> favoritesWithPaging = new PageImpl<>(favorites);
+        given(matchFavoriteRepository.findAllByMember(member, pageRequest)).willReturn(favoritesWithPaging);
+
+        //when
+        MatchFindAllWithPagingResponseDto result = matchService.findFavoriteMatchesPosts(0, member);
+
+        //then
+        assertThat(result.getMatches().size()).isEqualTo(1);
+
+    }
+
+    @DisplayName("매칭글 상태변경 테스트")
+    @Test
+    void changeMatchStatusTest() {
+        //given
+        Member member = createMember();
+        Match match = createMatch(member);
+        given(matchRepository.findById(1L)).willReturn(Optional.of(match));
+
+        //when
+        String result = matchService.changeMatchStatus(member, 1L);
+
+        //then
+        assertThat(result).isEqualTo("매칭글 상태를 변경하였습니다.");
+        assertThat(match.isMatched()).isTrue();
+    }
+}
