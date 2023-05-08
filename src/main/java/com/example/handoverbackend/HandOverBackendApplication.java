5440aaa742f10f1ef7a5cc8b8d247e007db22f14
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-05-08 23:45:47 +0900

    refactor : yml 민감정보를 감추기 위한 .env파일 생성

--- src/main/java/com/example/handoverbackend/HandOverBackendApplication.java
+++ src/main/java/com/example/handoverbackend/HandOverBackendApplication.java
@@ -2,10 +2,12 @@ package com.example.handoverbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource("classpath:/.env")
 public class HandOverBackendApplication {
 
     public static void main(String[] args) {
