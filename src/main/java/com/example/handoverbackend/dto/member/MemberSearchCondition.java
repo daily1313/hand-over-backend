42d36609e81e70d9ef8f4fc24542e0a5f47ef729
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-05-10 02:49:57 +0900

    chore : querydsl 관련 파일 주석 처리 및 제거

--- src/main/java/com/example/handoverbackend/dto/member/MemberSearchCondition.java
+++ src/main/java/com/example/handoverbackend/dto/member/MemberSearchCondition.java
@@ -1,15 +1,14 @@
-package com.example.handoverbackend.dto.member;
-
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MemberSearchCondition {
-
-    private String name;
-    private String nickname;
-}
+//package com.example.handoverbackend.dto.member;
+//
+//import lombok.AccessLevel;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+////@Getter
+////@AllArgsConstructor
+////@NoArgsConstructor(access = AccessLevel.PROTECTED)
+////public class MemberSearchCondition {
+////    private String name;
+////    private String nickname;
+////}
