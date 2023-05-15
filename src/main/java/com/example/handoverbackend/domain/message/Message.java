57cd387b1c918ccb0b501c3fea4a2eb9854d36d3
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-05-16 02:52:10 +0900

    refactor : Message 도메인 Column명 지정

--- src/main/java/com/example/handoverbackend/domain/message/Message.java
+++ src/main/java/com/example/handoverbackend/domain/message/Message.java
@@ -21,6 +21,7 @@ public class Message extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
     private Long id;
 
     @Column(nullable = false)
